66023a9daad71222f431c51d228881b2e6c1e770
annisatahira <annisatahira999@gmail.com>
2020-07-02 16:21:53 +0700

    change folder

--- src/data/travel-kit.js
+++ src/data/travel-kit.js
@@ -3,19 +3,19 @@ const introItems = [
     title: "Essential",
     description:
       "Start by collecting all of your important documents together, this will help ensure you have everything you need to get from one place to the next.",
-    image: "src/images/travel-kit-item-01.png"
+    image: "src/images/travel-kit/travel-kit-item-01.png"
   },
   {
     title: "Gadgets",
     description:
       "Traveling without tech items like your phone, laptop, tablet, or camera can be a major bummer. So don't forget your lovely gadgets",
-    image: "src/images/travel-kit-item-02.png"
+    image: "src/images/travel-kit/travel-kit-item-02.png"
   },
   {
     title: "Personal",
     description:
       "When packing for a vacation the most important things to keep in mind are the length of your trip, the weather, and any non-standard clothing you might need.",
-    image: "src/images/travel-kit-item-03.png"
+    image: "src/images/travel-kit/travel-kit-item-03.png"
   }
 ];
 
