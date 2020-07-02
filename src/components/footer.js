be1a38bc40e8f38c4b5bfd7ec6ec14ec72d8b143
annisatahira <annisatahira999@gmail.com>
2020-07-02 16:19:23 +0700

    fix content footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,26 +9,25 @@ class Footer extends HTMLElement {
     <div class="container">
       <div class="row">
         <div class="col l6 s12">
-          <h5 class="white-text">Footer Content</h5>
+          <h5 class="white-text">Travel Kit</h5>
           <p class="grey-text text-lighten-4">
-            You can use rows and columns here to organize your footer content.
+            Plan Your Journey and Discovering New Insight
           </p>
         </div>
         <div class="col l4 offset-l2 s12">
-          <h5 class="white-text">Links</h5>
+          <h5 class="white-text">Inspiration and Photo</h5>
           <ul>
-            <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-            <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-            <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-            <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+            <li>Pinterest - Photo by Jim Tobeo on behance</li>
+            <li>Pinterest - Photo by Nicepage</li>
+            <li>Canva</li>
           </ul>
         </div>
       </div>
     </div>
     <div class="footer-copyright">
       <div class="container">
-        © 2014 Copyright Text
-        <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+        © 2020 Submission PWA
+        <a class="grey-text text-lighten-4 right" href="#!">Annisa Tahira</a>
       </div>
     </div>
   </footer>`;
