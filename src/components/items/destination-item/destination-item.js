42cb5c58afc72c71d103cc5e399ee168f2e2bd14
annisatahira <annisatahira999@gmail.com>
2020-07-02 14:20:57 +0700

    fix travel destination style

--- src/components/items/destination-item/destination-item.js
+++ src/components/items/destination-item/destination-item.js
@@ -8,8 +8,17 @@ class DestinationItem extends HTMLElement {
     this.innerHTML = `
     <div class="col s12 m6 l6">
           <div id="place-item">
-            <h2>${this._data.name}</h2>
-            <img src="${this._data.image}" />
+            <div class="card">
+              <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="${this._data.image}">
+              </div>
+          
+              <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">${this._data.nation}<i class="material-icons right">close</i></span>
+                <p>${this._data.description}</p>
+              </div>
+            </div>
+            <h2>${this._data.city}</h2>
           </div>
     </div>`;
   }
--- src/data/destination.js
+++ src/data/destination.js
@@ -1,27 +1,45 @@
 const listDestination = [
   {
     image: "src/images/destination/bangkok.jpg",
-    name: "Thailand"
+    city: "Bangkok",
+    nation: "Bangkok, Thailand",
+    description:
+      "Ranked the world's most visited city in 2019 for the fourth year in a row, Thailand's vibrant, booming capital city is full of both ancient and modern culture as well as an incredible street food scene. Be sure to check out the city's historic temples and take in the splendor of the Grand Palace — and don't forget to stop by the famous floating markets for the city's most delectable eats."
   },
   {
     image: "src/images/destination/france.jpg",
-    name: "France"
+    city: "Lille",
+    nation: "Lille, France",
+    description:
+      "This charming northern city in France is quickly on the rise as a cultural metropolis, thanks to its renowned arts scene and impressive advances in urban development that helped it earn its recent crown as the World Design Capital of 2020. Enjoy art-forward museums like the curious La Piscine Museum while in Lille, as well as many superb restaurants and boutiques."
   },
   {
     image: "src/images/destination/portlans.jpg",
-    name: "Portland"
+    city: "Portland",
+    nation: "Portland, Maine",
+    description:
+      "There's more to Maine's largest city and buzzing cultural hub than its famous lighthouse-strewn coastline — Portland also boasts an incredible culinary scene full of great seafood and craft beer, as well as an excellent array of museums and galleries to visit. For a taste of New England charm, stroll down Portland's cobblestone streets and explore craft shops, bookstores, and breweries."
   },
   {
     image: "src/images/destination/british.jpg",
-    name: "Britain"
+    city: "British",
+    nation: "British Virgin Islands",
+    description:
+      "After a devastating hurricane struck the islands in 2017, the British Virgin Islands are making a full rebound in 2020 with the reopening of many major resorts. Whether you take in the crystal blue waters of the Caribbean or traverse through the picturesque green mountains across the islands (or both!), it's the perfect sun-soaked beach getaway you'll need in 2020."
   },
   {
     image: "src/images/destination/galway.jpg",
-    name: "Galway"
+    city: "Galway",
+    nation: "Galway, Ireland",
+    description:
+      "Just named the European Capital of Culture for 2020 (along with Rijeka, Croatia), this charming and vibrant city on Ireland's west coast will be a lively site for many cultural events in music, theatre, literature, and art this year — plus, you can enjoy a buzzing food hub as well as stunning scenery full of cobblestone streets and rocky landscapes."
   },
   {
     image: "src/images/destination/dubai.jpg",
-    name: "Dubai"
+    city: "Dubai",
+    nation: "Dubai, United Arab Emirates",
+    description:
+      "If there ever was a metropolis of the future, it's Dubai — this sprawling desert city boasts luxury hotels, sprawling malls, and even the tallest building in the world. Plus, 2020 will see Dubai host the first World Expo to be held in the Middle East, where over 190 countries will take part in showcasing their latest developments in sustainability and transportation. Hello, future!"
   }
 ];
 const listSeason = [
